606656ae775b3ecd675e118edd4cac737a47f622
Damian Mainka <maniek9472@gmail.com>
2018-03-18 22:27:58 +0100

    Naprawiony master

--- app/src/main/java/com/example/dmain/gymfit/Activity_New_Exercise.java
+++ app/src/main/java/com/example/dmain/gymfit/Activity_New_Exercise.java
@@ -1,6 +1,7 @@
 package com.example.dmain.gymfit;
 
-import android.support.design.widget.TextInputLayout;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,46 +10,91 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dmain.gymfit.database.DatabaseHelper;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Activity_New_Exercise extends AppCompatActivity {
 
 
-    private ArrayList<String> arrayList;
-    private ArrayAdapter<String> adapter;
-    private EditText txtInput;
-    private EditText txtInput2;
+
+
+
     int weight = 1,reps=1,series=1;
 
-    public void onCreate(Bundle savedInstanceState) {
+
+
+    DatabaseHelper myDB;
+    Button btnAdd,btnView;
+    EditText editText,editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__new__exercise);
+        editText = (EditText) findViewById(R.id.txtinput);
+        editText2 = (EditText) findViewById(R.id.txtinput2);
+        btnAdd = (Button) findViewById(R.id.btadd);
+
+        final ListView listView = (ListView) findViewById(R.id.listv);
+        myDB = new DatabaseHelper(this);
+        ArrayList<String> theList = new ArrayList<>();
+        Cursor data = myDB.getListContents();
+        if(data.getCount() == 0){
+            Toast.makeText(this, "There are no contents in this list!",Toast.LENGTH_LONG).show();
+        }else{
+            while(data.moveToNext()){
+                theList.add(data.getString(1));
+                ListAdapter listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,theList);
+                listView.setAdapter(listAdapter);
+            }
+        }
 
 
-        ListView listView = (ListView) findViewById(R.id.listv);
-        String[] items = {};
-        arrayList = new ArrayList<>(Arrays.asList(items));
-        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, arrayList);
-        listView.setAdapter(adapter);
-        txtInput = (EditText) findViewById(R.id.txtinput);
-        txtInput2 = (EditText) findViewById(R.id.txtinput2);
-        Button btAdd = (Button) findViewById(R.id.btadd);
 
-        btAdd.setOnClickListener(new View.OnClickListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newItem = txtInput.getText().toString();
-                String newItem2 = txtInput2.getText().toString();
-                arrayList.add(series +"  "+ newItem2 +"REPS  "+ newItem +"KG");
-                adapter.notifyDataSetChanged();
-                series++;
+                String newEntry = editText.getText().toString();
+                if(editText.length()!= 0){
+                    AddData(newEntry);
+                    editText.setText("");
+                }else{
+                    Toast.makeText(Activity_New_Exercise.this, "You must put something in the text field!", Toast.LENGTH_LONG).show();
+                }
+
+
+                String newEntry2 = editText2.getText().toString();
+                if(editText2.length()!= 0){
+                    AddData(newEntry2);
+                    editText2.setText("");
+                }else{
+                    Toast.makeText(Activity_New_Exercise.this, "You must put something in the text field!", Toast.LENGTH_LONG).show();
+                }
+
+
 
             }
         });
     }
+
+
+
+
+    public void AddData(String newEntry) {
+
+        boolean insertData = myDB.addData(newEntry);
+
+        if(insertData==true){
+            Toast.makeText(this, "Data Successfully Inserted!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Something went wrong :(.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 
     public void increaseWeight(View view) {
         weight = weight + 1;
