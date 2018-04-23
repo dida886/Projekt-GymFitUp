ef112b52c1ec57d79573feebc787b7ab9d9731ec
Damian Mainka <maniek9472@gmail.com>
2018-04-23 21:14:37 +0200

    do poprawki

--- app/src/main/java/com/example/dmain/gymfit/database/Series_Table.java
+++ app/src/main/java/com/example/dmain/gymfit/database/Series_Table.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
  * Created by dmain on 16.03.2018.
  */
 
-public class Series_Table extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "GYMFIT.db";
+public class Series_Table  {
+
     public static final String TABLE_NAME = "SERIES";
     public static final String ID_COL = "ID" ;
     public static final String WEIGHT_COL = "WEIGHT";
@@ -26,13 +26,11 @@ public class Series_Table extends SQLiteOpenHelper {
 
 
 
-    public Series_Table(Context context) {
-        super(context, TABLE_NAME, null, 56);
-    }
 
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
+
+
+    public static String createTable() {
         String createTable = "CREATE TABLE " + Series_Table.TABLE_NAME +
                 " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 Series_Table.WEIGHT_COL +" TEXT, "
@@ -41,32 +39,33 @@ public class Series_Table extends SQLiteOpenHelper {
                 + Series_Table.DATE_COL +" INT,"
                 + Series_Table.EXERCISE_ID_COL + " INT)";
 
-        db.execSQL(createTable);
+        return createTable;
     }
 
 
 
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
-    }
 
-    public Serie addDataSeries(String Weight, String Reps, String Time, int exercise_id, String Date) {
-        SQLiteDatabase db = this.getWritableDatabase();
+
+
+    public int insertSerie (Serie se) {
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(Series_Table.WEIGHT_COL, Weight);
-        contentValues.put(Series_Table.REPS_COL, Reps);
-        contentValues.put(Series_Table.TIME_COL, Time);
-        contentValues.put(Series_Table.EXERCISE_ID_COL, exercise_id);
-        contentValues.put(Series_Table.DATE_COL, Date);
+        contentValues.put(Series_Table.WEIGHT_COL, se.getWeight());
+        contentValues.put(Series_Table.REPS_COL, se.getReps());
+        contentValues.put(Series_Table.TIME_COL, se.getTime());
+        contentValues.put(Series_Table.EXERCISE_ID_COL, se.getExercise_id());
+        contentValues.put(Series_Table.DATE_COL, se.getDate());
         long result = db.insert(TABLE_NAME, null, contentValues);
 
-        return new Serie(Long.toString(result), Weight, Reps, Time,Date);
+
+
+
+        return new Serie(Long.toString(result), se.getWeight(), se.getReps(), se.getTime(),se.getDate(),se.getExercise_id());
+        DatabaseManager.getInstance().closeDatabase();
     }
-    public ArrayList<Serie> getListContentsSeries(int exercise_id){
-        SQLiteDatabase db = this.getWritableDatabase();
+    public ArrayList<Serie> getSeries(int exercise_id){
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE exercise_id = ?", new String[] {Integer.toString(exercise_id)});
 
         ArrayList<Serie> result = new ArrayList<Serie>();
@@ -93,11 +92,11 @@ public class Series_Table extends SQLiteOpenHelper {
             Serie s = new Serie(itemID, itemWeight, itemReps, itemTime,itemDate);
             result.add(s);
         }
-
+        DatabaseManager.getInstance().closeDatabase();
         return result;
     }
-    public ArrayList<Serie> getListContentsSeriesDate(String date){
-        SQLiteDatabase db = this.getWritableDatabase();
+   /* public ArrayList<Serie> getListContentsSeriesDate(String date){
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
         Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE date = ?", new String[] {new String(date)});
 
         ArrayList<Serie> result = new ArrayList<Serie>();
@@ -124,7 +123,8 @@ public class Series_Table extends SQLiteOpenHelper {
             Serie sd = new Serie(itemID, itemWeight, itemReps, itemTime,itemDate);
             result.add(sd);
         }
-
+        DatabaseManager.getInstance().closeDatabase();
         return result;
+
     }
-}
+}*/
